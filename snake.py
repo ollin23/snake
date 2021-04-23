278157161b4e84906228bb0c4747b99facfba6b5
ollin <balam.ariya@gmail.com>
2021-04-22 18:15:44 -0700

    Initial commit

---
+++ snake.py
@@ -0,0 +1,185 @@
+# from agents import *
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from pygame.locals import *
+import pygame
+import random
+import sys
+import time
+
+
+# RGB colors
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+
+# parameters
+WIDTH = 800
+HEIGHT = 600
+FPS = 15
+SIZE = 20
+
+
+def initialize():
+    pygame.init()
+    env = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    snake = [[int(WIDTH / 2), int(HEIGHT / 2)]]
+    x_range = (WIDTH - SIZE) // SIZE
+    y_range = (HEIGHT- SIZE) // SIZE
+    food = [random.randint(0, x_range) * SIZE, random.randint(0, y_range)*SIZE]
+
+    return env, snake, food
+
+
+def draw_snake(env, obj, color, size):
+    for x, y in obj:
+        pygame.draw.rect(env, color, [x, y, size, size])
+
+
+def spawn(env, target, snake, respawn=False):
+
+    if respawn:
+        x_range = (WIDTH - SIZE) // SIZE
+        y_range = (HEIGHT - SIZE) // SIZE
+        loc = [random.randint(0, x_range) * SIZE, random.randint(0, y_range) * SIZE]
+        while loc in snake:
+            loc = [random.randint(0, x_range) * SIZE, random.randint(0, y_range) * SIZE]
+
+        return loc
+    else:
+        # pygame.draw.rect(env, GREEN, [food[0], food[1], SIZE, SIZE])
+        pygame.draw.circle(surface=env,
+                           color=GREEN,
+                           center=[target[0]+10, target[1]+10],
+                           radius=SIZE//2,
+                           width=0)
+
+
+def detect_collision(head, snake, food):
+    chit = None
+    if head == food:
+        print("snek haz et")
+        chit = True
+
+    # boundary collisions
+    if head[0] > WIDTH-SIZE or head[0] < 0:
+        print("out of bounds: x-axis")
+        chit = False
+    if head[1] > HEIGHT-SIZE or head[1] < 0:
+        print("out of bounds: y-axis")
+        chit = False
+
+    # self collision
+    if len(snake) > 3:
+        if head in snake[1:]:
+            print("self collision")
+            chit = False
+
+    return chit
+
+
+def move(direction):
+    dx, dy = 0, 0
+    if direction == 0:
+        dy = -SIZE
+    if direction == 1:
+        dx = -SIZE
+    if direction == 2:
+        dy = SIZE
+    if direction == 3:
+        dx = SIZE
+
+    return dx, dy
+
+
+def display_location(snake):
+    x, y = snake[0]
+    print(f"length {len(snake)}; location: {x}, {y}")
+
+
+def play_game(env, food, snake, manual=True):
+    clock = pygame.time.Clock()
+
+    score = 1
+    direction = 3
+    dx = SIZE
+    dy = 0
+
+    loop = True
+    spawn(env, food, snake)
+    while loop:
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                print("Quit game")
+                loop = False
+                break
+
+        # manual interface
+        if manual:
+            pressed = pygame.key.get_pressed()
+            if pressed[K_UP] and direction != 2:
+                direction = 0
+                dx, dy = 0, -SIZE
+            if pressed[K_LEFT] and direction != 3:
+                direction = 1
+                dx, dy = -SIZE, 0
+            if pressed[K_DOWN] and direction != 0:
+                direction = 2
+                dx, dy = 0, SIZE
+            if pressed[K_RIGHT] and direction != 1:
+                direction = 3
+                dx, dy = SIZE, 0
+            else:
+                pass
+        else:
+            pass
+
+
+        # collision detection
+        head = [snake[0][0] + dx, snake[0][1] + dy]
+        chit = detect_collision(head, snake, food)
+        if chit:
+            score += 1
+            food = spawn(env, food, snake, True)
+            snake.insert(0, head)
+        elif chit is not None:
+            loop = False
+            break
+
+        # snake movement
+        snake.insert(0, head)
+        snake.pop()
+
+        # refresh env
+        env.fill(BLACK)
+        spawn(env, food, snake)
+        draw_snake(env, snake, RED, SIZE)
+        clock.tick(FPS)
+
+    return score
+
+
+def main():
+    # PARAMETERS
+    scores = []
+    iterations = 1
+
+    # initialize game
+    env, snake, food = initialize()
+
+    for i in range(iterations):
+        score = play_game(env, food, snake)
+        scores.append(score)
+
+    history = pd.DataFrame(data=scores, columns=["scores"])
+    print(history)
+
+
+if __name__ == "__main__":
+    main()
